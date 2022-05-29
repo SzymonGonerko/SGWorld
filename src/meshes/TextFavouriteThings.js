3c574d7dfb1424af51881e9d944d3caa6068ef5b
SzymonGonerko <szymon.gonerko@gmail.com>
2022-05-29 21:54:47 +0200

    added text kalimba

--- src/meshes/TextFavouriteThings.js
+++ src/meshes/TextFavouriteThings.js
@@ -2,7 +2,7 @@ import {Text} from "@react-three/drei";
 import OpenSans from "../fonts/OpenSans.ttf"
 
 const plotOfTheBookThief = "This is the story told by The Death. The protagonist is little girl, Liesel Meminger, who live's during the Second World War. The Death describes the adventures and feelings of little Liesel. Her friendship with a Jew, the story of her first kiss. Death in this book is not bad. He's just an observer. What I like most about this book is that it leaves behind all seemingly important things and just focuses on little Liesel's inner world of feelings."
-
+const kalimbaDescription = "For as long as I can remember, I liked instruments. In my collection I have a cajon, hulusi but Kalimba is my best. It's an instrument of African origin. Sometimes I play simple tunes before sleep. Sound is very gentle and pleasant. Kalimba coil is wedded in birth and marriage rituals."
 
 export const TextFavouriteThings = () => {
     const initialStyles = {
@@ -18,6 +18,18 @@ export const TextFavouriteThings = () => {
         ...initialStyles,
         fontSize: 0.1,
         letterSpacing: 0.07,
+    }
+    const textKalimba = {
+        ...initialStyles,
+        fontSize: 0.1,
+        letterSpacing: 0.07,
+    }
+    const textKalimbaDesc = {
+        ...initialStyles,
+        fontSize: 0.04,
+        letterSpacing: 0.07,
+        maxWidth: 0.8,
+        textAlign: "justify"
     }
     const textTitle = {
         ...initialStyles,
@@ -42,6 +54,10 @@ export const TextFavouriteThings = () => {
                 <Text {...textAuthor} text={"Markus Zusak,"} position={[0, 0, 0]}/>
                 <Text {...textTitle} text={'"The Book Thief"'} position={[-0.01, -0.11, 0]}/>
                 <Text {...textPlot} text={plotOfTheBookThief} position={[-0.01, -0.45, 0]}/>
+            </group>
+            <group position={[0, 0.45, 0]}>
+                <Text {...textKalimba} text={"Kalimba"} position={[0, 0, 0]}/>
+                <Text {...textKalimbaDesc} text={kalimbaDescription} position={[-0.01, -0.346, 0]}/>
             </group>
             
 
